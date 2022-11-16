f031cfb76e01be0dd1357b293e9bb4226f253f4f
jasdj <jasdj@JDJ.dhcp.wustl.edu>
2022-11-16 15:45:19 -0600

    studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,69 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
 
+	/**
+	 * @param month - what month it is
+	 */
+	private int month;
+	
+	/**
+	 * @param day - what day it is, from 1 to 31
+	 */
+	private int day;
+	
+	/**
+	 * @param year - what year it is
+	 */
+	private int year;
+	
+	/**
+	 * @param holiday - if that day is a holiday
+	 */
+	private boolean holiday;
 
-    public static void main(String[] args) {
+	public Date(int initMonth, int initDay, int initYear, boolean initHoliday) {
+		
+		month = initMonth;
+		day = initDay;
+		year = initYear;
+		holiday = initHoliday;
+		
+	}
+	
+	public String toString(){
+		return this.month + " " + this.day + "," + this.year;
+		
+	}
+	
+	
+	
+    @Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && Objects.equals(month, other.month) && year == other.year;
+	}
+
+	public static void main(String[] args) {
+
+		Date start = new Date(1, 1, 2000, false);
+		Date end = new Date(12, 31, 2000, false);
+		System.out.println(start.equals(end));
+		System.out.println(start.equals(start));
+		
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,61 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
-
+	
+	/**
+	 * @param hour - takes in hour in the 24 hour format
+	 */
+	private int hour;
+	
+	/**
+	 * @param minute - takes in amount of minutes, from 0 to 59
+	 */
+	private int minute;
+	
+	/*
+	 * @param format - if someone wants the time in the 12 hour format or not
+	 */
+	private boolean format;
+	
+	
+	public Time(int initHour, int initMinute, boolean initFormat) {
+		
+		hour = initHour;
+		minute = initMinute;
+		format = initFormat;
+		
+	}
+	
+	public String toString() {
+		return this.hour + ":" +  this.minute;
+	}
+	
 	public static void main(String[] args) {
  
+		Time noon = new Time(12, 0, false);
+		Time evening = new Time(20, 0, false);
+		System.out.println(noon.equals(evening));
+		System.out.println(evening.equals(evening));
     	
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
 
 }
